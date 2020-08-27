6abb4cd8c0a4a2050cb5e59fcb0e0386c3b9a479
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-08-27 17:18:08 +0200

    element & element.all, by.xpath & by.css

--- src/tests/first-protractor-test.spec.ts
+++ src/tests/first-protractor-test.spec.ts
@@ -1,9 +1,18 @@
-import { browser } from 'protractor';
+import { browser, element, by, ElementFinder, ElementArrayFinder } from 'protractor';
 
 describe('First protractor test', () => {
-  it('should navigate to Google', () => {
-    browser.get('https://google.com');
+  it('should navigate to our application', () => {
+    // Navigate to application
+    browser.get('http://localhost:4200');
 
-    expect(true).toBe(true);
+    const headings: ElementArrayFinder = element.all(by.css('.well.hoverwell.thumbnail>h2'));
+    // Holds an array of all elements found by the css selector
+    headings.getText()
+      // headingTexts is an array of strings containing the heading texts
+      .then(headingTexts => console.log('Headings: ', headingTexts));
+
+    // Find a specific heading and click it
+    const heading: ElementFinder = element(by.xpath('//app-course-thumbnail/div/h2[text()=\' Selenium Framework development \']'));
+    heading.click();
   });
 });
