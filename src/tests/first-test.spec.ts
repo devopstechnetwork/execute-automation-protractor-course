04a344c7bd789bf6003fad2244f48a6202faabeb
Jarrod Kallis <jarrod.kallis@gmail.com>
2020-08-27 15:06:34 +0200

    Jasmine tests

---
+++ src/tests/first-test.spec.ts
@@ -0,0 +1,11 @@
+describe('First Test', () => {
+  it('should pass', () => {
+    expect(true).toBe(true);
+  });
+
+  it('should be undefined', () => {
+    // const a: any = undefined;
+
+    expect(undefined).toBeUndefined();
+  });
+});
---
+++ src/tests/second-test.spec.ts
@@ -0,0 +1,7 @@
+describe('Second test', () => {
+  it('should pass again', () => {
+    const a = 12;
+
+    expect(a).toBe(12);
+  });
+});
